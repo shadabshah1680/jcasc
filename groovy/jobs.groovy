7a68d9ce6d4b0d9a5ce2843fd58884ca91f1194e
EC2 Default User <ec2-user@ip-172-31-52-49.ec2.internal>
2022-09-01 00:44:32 +0000

    force push

---
+++ groovy/jobs.groovy
@@ -0,0 +1,24 @@
+// theScript = new File('/var/jenkins_conf/test_pipeline.groovy').text
+// pipelineJob('Test') {
+//   definition {
+//     cps {
+//       script(theScript)
+//       sandbox()
+//     }
+//   }
+// }
+
+    
+// listView("SlackTest") {
+//     jobs {
+//         name('Test')
+//     }
+//     columns {
+//         status()
+//         weather()
+//         name()
+//         lastSuccess()
+//         lastFailure()
+//         lastDuration()
+//       }
+// }
--- jobs/test_pipeline.groovy
+++ jobs/test_pipeline.groovy
@@ -1,10 +1,47 @@
-pipeline {
-    agent any
-    stages { 
-        stage('Example') {
-            steps {
-                echo 'Hello World'
-            }
+pipelineJob('slack_shared_library_configuration_with_jcasc') {
+    definition {
+        cps {
+            script('''
+
+node {
+
+    stage("STARTED") {
+    
+					try{
+					        slackNotifier('STARTED')
+                  echo "STARTED"
+         }
+					catch(a) {
+					        currentBuild.result = 'FAILURE'	
+         }
+    
+    } 
+       
+    stage("SUCCESS") {
+
+					try{
+					        slackNotifier('SUCCESS')
+                  echo "SUCCESS"
+         }
+					catch(b) {
+					        currentBuild.result = 'FAILURE'	
+         }
+			    
+   }
+
+    stage("ERROR") {
+        	
+					try{
+					        slackNotifier('UNSTABLE')
+                  echo 'ERROR'
+         }
+					catch(c) {
+					        currentBuild.result = 'FAILURE'	
+         }
+    }
+}
+            ''')
+            sandbox()
         }
     }
 }
