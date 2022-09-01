1808f2d3c8ed2342f27f6de0d6068cf672fcbbd3
EC2 Default User <ec2-user@ip-172-31-52-49.ec2.internal>
2022-09-01 21:15:47 +0000

    new

--- groovy/jobs.groovy
+++ groovy/jobs.groovy
@@ -22,3 +22,22 @@
 //         lastDuration()
 //       }
 // }
+
+
+// def githubUrl = "https://github.com/m-goos/jenkins-jobdsl-seedrepo-example.git"
+
+// pipelineJob("Seed job 1") {
+// definition {
+//     cpsScm {
+//         scm {
+//             git{
+//               remote {
+//                 url("${githubUrl}")
+//                 // credentials("${SSH_CREDENTIALS}")
+//               }
+//               branch("*/do-not-merge")
+//             }
+//         }
+//     }
+//   }
+// }
